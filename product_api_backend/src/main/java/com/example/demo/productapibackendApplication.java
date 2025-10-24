3f0e7f374faf32e0fdcfabbc62744673f5d443df
Kavia AI <production@kavia.ai>
2025-10-24 17:40:56 +0000

    CheckPoint - cg083c1802

---
+++ product_api_backend/src/main/java/com/example/demo/productapibackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.productapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class productapibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(productapibackendApplication.class, args);
+	}
+
+}
